4d43cf648cae98003df9194f75fd06243019bb1b
Andrey <an.nikishin2012@yandex.ru>
2023-04-11 17:37:14 +0300

    Решены 4 задачи

---
+++ Zadacha_2.py
@@ -0,0 +1,2 @@
+i = input()
+print(int(i[0])+int(i[1])+int(i[2]))
---
+++ Zadacha_6.py
@@ -0,0 +1,7 @@
+S = input()
+a = int(S[0]) + int(S[1]) + int(S[2])
+b = int(S[3]) + int(S[4]) + int(S[5])
+if a == b:
+    print('Yes')
+else:
+    print("No")
